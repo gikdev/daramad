484998962a6416cbcbc435fbcf031f0e8299d54f
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-05-07 00:18:28 +0330

    Still working on cards...

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,16 @@
-import { House, Minus, Plus, Prohibit, Trash } from "@phosphor-icons/react"
+import { CircleDashed, House, Minus, Plus, Prohibit } from "@phosphor-icons/react"
+import { useState } from "react"
 import { Link } from "react-router"
 // import * as rdx from "#/assets/radix-colors-dark"
 
+function toPersianNumerals(input: string): string {
+  const persianDigits = '۰۱۲۳۴۵۶۷۸۹';
+  return input.replace(/\d/g, (d) => persianDigits[Number.parseInt(d, 10)]);
+}
+
 export default function Home() {
+  const [price, setPrice] = useState(3_200)
+
   return (
     <div className="min-h:100dvh flex flex-direction:column">
       {/* <nav className="bg:rdx-slate-20 flex jc:space-between border-bottom:1 border:rdx-slate-60">
@@ -30,75 +38,43 @@ export default function Home() {
         </button>
       </nav> */}
 
-      <main className="flex:1|1|0 p:5x flex flex-direction:column gap:3x">
+      <main className="flex:1|1|0 p:5x flex flex-direction:column gap:3x ai:center">
         <h1 className="text:center text:rdx-slate-120 font-size:x-large font-weight:900">
           مدیریت بودجه
         </h1>
 
-        <div className="bg:rdx-slate-30 p:3x r:8 flex flex-direction:column gap:4x">
-          <div className="flex gap:1x ai:center">
-            <span className="margin-inline-end:auto text:rdx-slate-110">تفریح</span>
-
-            <button
-              type="submit"
-              className="fill:rdx-green-100 bg:rdx-green-40 p:1x r:4 cursor:pointer transform:scale(0.9):active"
-            >
-              <Plus size={24} className="fill:inherit" />
-            </button>
-
-            <button
-              type="submit"
-              className="fill:rdx-amber-100 bg:rdx-amber-40 p:1x r:4 cursor:pointer transform:scale(0.9):active"
-            >
-              <Minus size={24} className="fill:inherit" />
-            </button>
-
-            <button
-              type="submit"
-              className="fill:rdx-red-100 bg:rdx-red-40 p:1x r:4 cursor:pointer transform:scale(0.9):active"
-            >
-              <Trash size={24} className="fill:inherit" />
-            </button>
-          </div>
-
-          <div className="">
-            <p className="" dir="ltr">
-              <span className="text:rdx-slate-120 font:bold font-size:xx-large">۳،۲۰۰</span>
-              <span className="text:rdx-slate-110">ت</span>
+        <div className="bg:rdx-slate-30 p:3x r:8 flex flex-direction:column gap:4x max-w:max-content w:full border:1|solid|rdx-slate-70">
+          <div className="flex flex-direction:column gap:2x">
+            <p className="text:rdx-slate-110 fill:rdx-slate-110 flex ai:center gap:1x">
+              <CircleDashed size={16} className="fill:inherit" />
+              <span>تفریح</span>
             </p>
-          </div>
-        </div>
 
-        <div className="bg:rdx-slate-30 p:3x r:8 flex flex-direction:column gap:4x">
-          <div className="flex gap:1x ai:center">
-            <span className="margin-inline-end:auto text:rdx-slate-110">تفریح</span>
-
-            <p className="" dir="ltr">
-              <span className="text:rdx-slate-120 font:bold font-size:xx-large">۳،۲۰۰</span>
+            <p className="w:full" dir="ltr">
+              <span className="text:rdx-slate-120 font:bold font-size:xx-large">{toPersianNumerals(Number(price).toLocaleString())}</span>
               <span className="text:rdx-slate-110">ت</span>
             </p>
           </div>
 
-          <div className="">
+          <input
+            className="border:2|solid|rdx-slate-70 px:4x py:2x py:1x r:8 flex:2|2|0 min-w:30x max-w:full bg:rdx-slate-40 text:rdx-slate-120"
+            dir="ltr"
+            type="number"
+          />
+
+          <div className="flex gap:2x flex-wrap:wrap">
             <button
-              type="submit"
-              className="fill:rdx-green-100 bg:rdx-green-40 p:2x r:8 cursor:pointer transform:scale(0.9):active"
+              type="button"
+              className="fill:rdx-green-100 bg:rdx-green-40 p:2x r:8 cursor:pointer transform:scale(0.9):active flex:1|1|0 min-w:max-content flex ai:center jc:center"
             >
-              <Plus size={36} className="fill:inherit" />
+              <Plus size={32} className="fill:inherit" />
             </button>
 
             <button
-              type="submit"
-              className="fill:rdx-amber-100 bg:rdx-amber-40 p:2x r:8 cursor:pointer transform:scale(0.9):active"
+              type="button"
+              className="fill:rdx-amber-100 bg:rdx-amber-40 p:2x r:8 cursor:pointer transform:scale(0.9):active flex:1|1|0 min-w:max-content flex ai:center jc:center"
             >
-              <Minus size={36} className="fill:inherit" />
-            </button>
-
-            <button
-              type="submit"
-              className="fill:rdx-red-100 bg:rdx-red-40 p:2x r:8 cursor:pointer transform:scale(0.9):active"
-            >
-              <Trash size={36} className="fill:inherit" />
+              <Minus size={32} className="fill:inherit" />
             </button>
           </div>
         </div>
