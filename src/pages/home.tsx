18db89eb42f00badaabdbf5763c4c40a2c4802e7
gikdev <m.m.bhrm85@gmail.com>
2025-05-07 20:42:52 +0330

    Made the card work

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,12 +4,13 @@ import { Link } from "react-router"
 // import * as rdx from "#/assets/radix-colors-dark"
 
 function toPersianNumerals(input: string): string {
-  const persianDigits = '۰۱۲۳۴۵۶۷۸۹';
-  return input.replace(/\d/g, (d) => persianDigits[Number.parseInt(d, 10)]);
+  const persianDigits = "۰۱۲۳۴۵۶۷۸۹"
+  return input.replace(/\d/g, d => persianDigits[Number.parseInt(d, 10)])
 }
 
 export default function Home() {
   const [price, setPrice] = useState(3_200)
+  const [sidePrice, setSidePrice] = useState(0)
 
   return (
     <div className="min-h:100dvh flex flex-direction:column">
@@ -51,21 +52,30 @@ export default function Home() {
             </p>
 
             <p className="w:full" dir="ltr">
-              <span className="text:rdx-slate-120 font:bold font-size:xx-large">{toPersianNumerals(Number(price).toLocaleString())}</span>
+              <span className="text:rdx-slate-120 font:bold font-size:xx-large">
+                {toPersianNumerals(Number(price).toLocaleString())}
+              </span>
               <span className="text:rdx-slate-110">ت</span>
             </p>
           </div>
 
           <input
-            className="border:2|solid|rdx-slate-70 px:4x py:2x py:1x r:8 flex:2|2|0 min-w:30x max-w:full bg:rdx-slate-40 text:rdx-slate-120"
+            min={0}
             dir="ltr"
             type="number"
+            value={sidePrice}
+            onChange={e => setSidePrice(Number(e.target.value))}
+            className="border:2|solid|rdx-slate-70 px:4x py:2x py:1x r:8 flex:2|2|0 min-w:30x max-w:full bg:rdx-slate-40 text:rdx-slate-120"
           />
 
           <div className="flex gap:2x flex-wrap:wrap">
             <button
               type="button"
               className="fill:rdx-green-100 bg:rdx-green-40 p:2x r:8 cursor:pointer transform:scale(0.9):active flex:1|1|0 min-w:max-content flex ai:center jc:center"
+              onClick={() => {
+                setPrice(p => p + sidePrice)
+                setSidePrice(0)
+              }}
             >
               <Plus size={32} className="fill:inherit" />
             </button>
@@ -73,6 +83,10 @@ export default function Home() {
             <button
               type="button"
               className="fill:rdx-amber-100 bg:rdx-amber-40 p:2x r:8 cursor:pointer transform:scale(0.9):active flex:1|1|0 min-w:max-content flex ai:center jc:center"
+              onClick={() => {
+                setPrice(p => p - sidePrice)
+                setSidePrice(0)
+              }}
             >
               <Minus size={32} className="fill:inherit" />
             </button>
